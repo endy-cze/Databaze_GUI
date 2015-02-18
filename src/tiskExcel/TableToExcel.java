031d141dc9523fb07a796a93c61b62a3139297a3
Kendysss6 <Kendysss6@users.noreply.github.com>
2015-02-18 21:40:37 +0100

    Porkrok v tisku 2
    Dodal jsem par metod a hlavně jsem přidal dokumentaci od souboru. trochu
    jsem upravil hierarchii a napsal jak budu postupovat do hlavičky třídy
    dokumentace.

--- src/tiskExcel/TableToExcel.java
+++ src/tiskExcel/TableToExcel.java
@@ -1,7 +1,7 @@
 package tiskExcel;
 
+import java.io.File;
 import java.io.FileOutputStream;
-import java.io.IOException;
 
 import javax.swing.table.TableModel;
 
@@ -15,34 +15,46 @@ import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.util.CellRangeAddress;
 
+/**
+ * Tøída pro export Table model do .xls souboru (Excel soubor). Návrh øešení problému:
+ * 1. vytvoøit metodu se switchem, která podle èísla vypisu navratí název sheetu, nadpis, cestu kde uložit vèetnì jména
+ * 2. vytvoøit metodu která nám vrátí kolik která bunka má šíøku a kolik øádek jsou nadpisy. 
+ * 	 možná jen boolean jestli je to nutné, protože jediny kdo potøebuje víc øádkù
+ *   je Licí plán - planovací - nezapomen doplnit poznamku do sloupce
+ * 3. vytvoøit metodu která vloží data do xls souboru  (insertData(.))
+ * Rozložení stránek (okraje stranky) budou pro všechny výpisy stejné
+ * @author Havlicek
+ *
+ */
 public class TableToExcel {
 	
 	/**
 	 * Vytvoøí tøídu, která vytvoøí .xls soubor do dané složky. Table model a èíslo výpisu spolu souvisí, jelikož
 	 * podle èísla výpisu se vybere daná šablona, do které se budou data vkládat.
 	 * @param model ze kterého èerpáme data
-	 * @param path kam chceme uložit soubor
+	 * @param name jméno souboru bez koncovky
 	 * @param cisloVypisu èíslo výpisu (èíslo šablony, kterou použijeme pro soubor xls)
 	 * @throws Exception vyhodí chybu, pokud nìco nesouhlasí
 	 */
-	public TableToExcel(TableModel model, String path, int cisloVypisu) throws Exception{
-		this.export(model, path, cisloVypisu);
+	public TableToExcel(TableModel model, String name, int cisloVypisu) throws Exception{
+		this.export(model, name, cisloVypisu);
 	}
 	
 	/**
-	 * Metoda, kde dìje hlavní algorytmus. Zde se vše pøevádí
+	 * Metoda, kde dìje hlavní algoritmus. Zde se vše pøevádí
 	 * @param model model, ze kretého èerpáme data
-	 * @param path kam chceme uložit .xls soubor (pokud možno relativní)
+	 * @param name nazev souboru bez koncovky
 	 * @param cisloVypis druh vypisu, podle kterého pøizpùsobíme .xls soubor
 	 * @throws Exception 
 	 */
-	private void export(TableModel model, String path, int cisloVypis) throws Exception{
+	private void export(TableModel model, String name, int cisloVypis) throws Exception{
 		HSSFWorkbook wb = new HSSFWorkbook();
-		HSSFSheet sheet = this.createSheet(wb, cisloVypis);
+		String [] atr = this.getAtributes(cisloVypis);
+		HSSFSheet sheet = wb.createSheet(atr[0]);
+		
 		
 		Footer footer = sheet.getFooter();
-		footer.setRight("Page " + HeaderFooter.page() + " of "
-				+ HeaderFooter.numPages());
+		footer.setRight("Page " + HeaderFooter.page() + " of " + HeaderFooter.numPages());
 		
 		sheet.setPrintGridlines(true);
 	    
@@ -57,9 +69,7 @@ public class TableToExcel {
 		sheet.setRepeatingRows(CellRangeAddress.valueOf("1:1"));
 		
 		
-		this.insertData(model, sheet, cisloVypis);
-		
-		Row row = sheet.createRow(0);
+			Row row = sheet.createRow(0);
 		Cell cell = row.createCell(0);
 		cell.setCellValue("Nadpis1");
 		cell = row.createCell(1);
@@ -70,28 +80,50 @@ public class TableToExcel {
 			cell.setCellValue("Skoda smrdí "+i);
 		}
 		
+		
+		this.insertData(atr[1], model, sheet, cisloVypis);
+		
+	
 
 		
-		// Write the output to a file
-		FileOutputStream fileOut = new FileOutputStream(path);
+		// Write it into the output to a file
+		
+		File f = new File("./vypisy");
+		try {
+			if (f.mkdir()) {
+				//System.out.println("Directory Created");
+			} else {
+				//System.out.println("Directory is not created");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		FileOutputStream fileOut = new FileOutputStream("./vypisy/"+name+".xls");
 		wb.write(fileOut);
+		wb.close();
 		fileOut.close();
 	}
 	
 	/**
 	 * Metoda pro vložení dat do tabulky, ještì bych mìl zvážit že pøidam parametr pro mergovaní bunìk.
 	 * Ještì musím vymyslet jak. Nìjaky jednoduhcy øešení. :)
+	 * @param nadpis napis v tisku
 	 * @param model ze kterého budeme èíst data
 	 * @param sheet do kterého budeme data vkládat
 	 * @param cisloVypis èíslo vypisu
-	 * @throws Exception 
+	 * @throws Exception
 	 */
-	private void insertData(TableModel model, HSSFSheet sheet, int cisloVypis) throws Exception{
+	private void insertData(String nadpis, TableModel model, HSSFSheet sheet, int cisloVypis) throws Exception{
 		HSSFRow fRow = (HSSFRow) sheet.getRow(sheet.getFirstRowNum());
 		int colCount = 0;
 		for(Cell cell : fRow){
 			colCount++;
 		}
+		System.out.println("celkem bunek: "+colCount);
+		colCount = sheet.getRow(0).getPhysicalNumberOfCells();
+		System.out.println("celkem bunek: "+colCount);
+		colCount = sheet.getRow(0).getLastCellNum();
 		System.out.println("celkem bunek: "+colCount);
 		if(model.getColumnCount() != colCount){
 			throw new Exception("Spatny pocet sloupcu v modelu nebo šablonì");
@@ -99,14 +131,25 @@ public class TableToExcel {
 	}
 	
 	/**
-	 * Metoda pro vytvoøení tøídy Sheet. Udìlí mu jméno podle èísla výpisu.
-	 * @param wb do kterého vytvoøí Sheet
-	 * @param cisloVypisu èíslo výpisu
-	 * @return Sheet s pøidìleným jménem
+	 * Metoda pro zjištìní atributù výpisu. Navrátí String pole o 3 prvcích v poøadí:
+	 * <ol>
+	 *  <li>Jméno sheetu</li>
+	 *  <li>Nadpis v tisku papíru</li>
+	 *  <li>Cestu kam uložit (relativní)</li>
+	 * </ol>
+	 * @param cisloVypisu Èíslo výpisu, o kterém chceme znát atributy
+	 * @return String [] o 3 prvcích nebo null pokud neexistuje
 	 */
-	private HSSFSheet createSheet(HSSFWorkbook wb, int cisloVypisu){
-		HSSFSheet sheet = wb.createSheet("new sheet");
-		return sheet;
+	private String [] getAtributes(int cisloVypisu){
+		String [] atr = null;
+		switch(cisloVypisu){
+		case 1:
+			String [] atr1 = {"Stav neuzavøených zakázek", "Stav neuzavøených zakázek", ".//"};
+			atr = atr1;
+			break;
+		case 2:
+			break;
+		}
+		return atr;		
 	}
-
 }
