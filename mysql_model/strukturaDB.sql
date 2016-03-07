84ebf46044905fc41c4291ff2a87b76cc2f31a0a
Kendy <nope@kendys.cz>
2016-03-07 20:14:46 +0100

    Minor changes in strukturaDB
    Jenom na zacatek dat use pomdb;

--- mysql_model/strukturaDB.sql
+++ mysql_model/strukturaDB.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `pomdb` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `pomdb`;
 -- MySQL dump 10.13  Distrib 5.6.23, for Win64 (x86_64)
 --
 -- Host: localhost    Database: pomdb
@@ -3347,4 +3349,4 @@ DELIMITER ;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-03-07 20:06:36
+-- Dump completed on 2016-03-07 20:12:30
